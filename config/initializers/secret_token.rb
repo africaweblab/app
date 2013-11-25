e58f7eee6aff8520c2634164c75a0f5ad687de4d
africaweblab <projet.ayamin@gmail.com>
2013-11-25 11:22:36 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Africaweblab::Application.config.secret_token = '0fcdd1c92cb534a8d9f3b0762cb49806614ce26909884f9c3f564e061c38d688dbeb72fb76ff7a2067bcf53605c69a9ddb2352d91afff97fd7be961b9fc5b00e'
